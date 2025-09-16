72b103baec3924cda30695d68773cdfd8a0eb75a
Eversoniananan <27harte@flintridgeprep.org>
2025-09-16 15:54:18 -0700

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWMap.java
@@ -1,8 +1,36 @@
+package org.firstinspires.ftc.teamcode.Projects;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 package org.firstinspires.ftc.teamcode;
 
 public class HWMap {
+    public DcMotor  LeftDrive   = null;
+    public DcMotor  RightDrive  = null;
 
+    public void init(HardwareMap hwMap) {
 
+        LeftDrive  = hwMap.get(DcMotor.class, "LeftDrive");
+        RightDrive = hwMap.get(DcMotor.class, "RightDrive");
+
+        LeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        RightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        RightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        RightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Stop();
+
+    }
+    public void Stop() {
+        RightDrive.setPower(0);
+        LeftDrive.setPower(0);
+    }
 
 }
 //jhnbg
