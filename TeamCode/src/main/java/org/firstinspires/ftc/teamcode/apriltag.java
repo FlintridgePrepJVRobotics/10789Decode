f2917b49e89b8ed35aed0653c97f53a93547739d
Eversoniananan <27harte@flintridgeprep.org>
2025-09-18 16:39:05 -0700

    finished april tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltag.java
@@ -1,7 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 
 @TeleOp
 public class apriltag extends LinearOpMode {
@@ -9,13 +16,34 @@ public class apriltag extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagOutline(true)
+                .build();
 
-//sdfghngfds
-
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
 
         waitForStart();
 
         while (!isStopRequested () && opModeIsActive()) {
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+            }
+            telemetry.update();
+
 
 
 
@@ -23,4 +51,3 @@ public class apriltag extends LinearOpMode {
         }
     }
 }
-
