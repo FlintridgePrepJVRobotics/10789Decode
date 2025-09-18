063bb6d934dd8cc824bba8eef05e8048de28bbf1
Eversoniananan <27harte@flintridgeprep.org>
2025-09-17 18:03:50 -0700

    cleaned teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWMap.java
@@ -5,45 +5,41 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 public class HWMap {
-    public DcMotor  LeftDrive   = null;
-    public DcMotor  RightDrive  = null;
-
-    public DcMotor  Intake  = null;
-    public DcMotor  Outake  = null;
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+    public DcMotor  intake  = null;
+    public DcMotor  outake  = null;
 
 
     public void init(HardwareMap hwMap) {
 
-        LeftDrive  = hwMap.get(DcMotor.class, "LeftDrive");
-        RightDrive = hwMap.get(DcMotor.class, "RightDrive");
-        Intake = hwMap.get(DcMotor.class, "Intake");
-        Outake = hwMap.get(DcMotor.class, "RightDrive");
+        leftDrive  = hwMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hwMap.get(DcMotor.class, "rightDrive");
+        intake = hwMap.get(DcMotor.class, "intake");
+        outake = hwMap.get(DcMotor.class, "rightDrive");
 
-        LeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        RightDrive.setDirection(DcMotor.Direction.REVERSE);
-        Intake.setDirection(DcMotor.Direction.FORWARD);
-        Outake.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.FORWARD);
+        outake.setDirection(DcMotor.Direction.REVERSE);
 
 
-        RightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        LeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        Outake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        outake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        RightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        LeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        Intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Outake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         Stop();
-//dfgjkdfgjkdfghjkasdasd
     }
     public void Stop() {
-        RightDrive.setPower(0);
-        LeftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftDrive.setPower(0);
     }
 
 }
-//jhnbg
