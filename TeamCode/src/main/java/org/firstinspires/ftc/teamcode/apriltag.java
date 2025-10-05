386ade7251359a90036942372e47c1b9718ba1b7
Eversoniananan <27harte@flintridgeprep.org>
2025-10-04 23:31:13 -0700

    point and spool function for 2 motor flywheel design, still need to code for a feeding mechanism or a delay if the flywheel auto loading.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltag.java
@@ -63,7 +63,7 @@ public class apriltag extends LinearOpMode {
 
                 drive(strafeError, forwardError, headingError);
 
-                spool(distanceToTag); // flywheel power based on distance
+                spool(distanceToTag); // spool flywheel
 
             } else {
                 // Manual control
