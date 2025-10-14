82b5355c40e5f98743dadbfd86ae00f01c8b7660
vwang <28wangv@flintridgeprep.org>
2025-10-14 16:33:30 -0700

    coded auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton.java
@@ -1,6 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
-public class auton {
-    //isaacs address: 2060 liliano dr.
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-}
+import org.firstinspires.ftc.teamcode.HWMap;
+
+//name that appears on the driver hub screen
+@Autonomous(name = "Basic Auto")
+public class auton extends LinearOpMode {
+    //making a robot from project file (hardware map)
+    public HWMap robot = new HWMap();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //initialize hardware map
+        robot.init(hardwareMap);
+
+        waitForStart();
+        forward(2500, 1);
+        forward(500, 0);
+    }
+
+    public void forward(int time, int speed) { //forward
+        robot.backRightDrive.setPower(speed);
+        robot.backLeftDrive.setPower(speed);
+        robot.frontRightDrive.setPower(speed);
+        robot.frontLeftDrive.setPower(speed);
+        sleep(time);
+    }
+
+
+    }
