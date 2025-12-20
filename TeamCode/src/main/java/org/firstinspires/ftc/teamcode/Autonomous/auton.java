e18dd3882998bb09f0475137f172b019d5461fa4
Eversoniananan <27harte@flintridgeprep.org>
2025-12-19 21:12:21 -0800

    shoot in place

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/auton.java
@@ -1,114 +1,222 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
-
+import org.firstinspires.ftc.teamcode.HWMap;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.teamcode.HWMap;
-
-//name that appeafrss on the driver hub screen
-@Autonomous(name = "shootinplace")
+@Autonomous(name = "HSOOTINPLACE")
 public class auton extends LinearOpMode {
-    //making a robot from project file (hardware map)
+
     public HWMap robot = new HWMap();
-    double power = 950;
+
+    // ===== SHOOTER CONSTANTS (same logic as TeleOp) =====
+    private static final double MOTOR_TICKS_PER_REV = 560.0;
+    private static final double MOTOR_MAX_RPM = 300.0;
+    private static final double SHOOTER_TO_MOTOR_RATIO = 4.0;
+
+    // ===== DRIVE CONSTANTS =====
+    private static final double DRIVE_TICKS_PER_REV = 560.0; // HD Hex
+    private static final double WHEEL_DIAMETER_IN = 3.78;    // CHANGE if needed
+    private static final double TICKS_PER_INCH =
+            DRIVE_TICKS_PER_REV / (Math.PI * WHEEL_DIAMETER_IN);
+
     @Override
-    public void runOpMode() throws InterruptedException {
-        //initialize hardware map
-        robot.init(hardwareMap);// start block
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        DcMotorEx flywheelOne = (DcMotorEx) robot.flywheelOne;
+        DcMotorEx flywheelTwo = (DcMotorEx) robot.flywheelTwo;
+
+        // Reset encoders
+        flywheelOne.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        flywheelTwo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        flywheelOne.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheelTwo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // PIDF (same as TeleOp)
+        flywheelOne.setVelocityPIDFCoefficients(10, 0.5, 0, 12);
+        flywheelTwo.setVelocityPIDFCoefficients(10, 0.5, 0, 12);
+
         waitForStart();
-        robot.flywheelOne.setVelocity(power);
-        robot.flywheelTwo.setVelocity(power);
-        sleep(3000);
-        robot.feedServo.setPosition(0);//number 1
-        sleep(2000);
+
+        if (!opModeIsActive()) return;
+
+        // ================= SHOOT =================
+        double shooterRPM = 400;
+        double motorRPM = shooterRPM / SHOOTER_TO_MOTOR_RATIO;
+        motorRPM = Math.min(motorRPM, MOTOR_MAX_RPM);
+
+        double targetTicksPerSec = motorRPM * MOTOR_TICKS_PER_REV / 60.0;
+
+        flywheelOne.setVelocity(targetTicksPerSec);
+        flywheelTwo.setVelocity(targetTicksPerSec);
+
+        sleep(1200); // spin-up time
+
+        robot.feedServo.setPosition(0); // fire
+        sleep(300);
         robot.feedServo.setPosition(1);
-        robot.flywheelOne.setPower(0);
-        robot.flywheelTwo.setPower(0);
-        sleep(1000);
-        robot.intake.setPower(0.5);
-        sleep(250);
-        robot.intake.setPower(-0.5);
-        sleep(500);
-        robot.intake.setPower(0);//end block
 
+        sleep(300);
 
-        robot.flywheelOne.setVelocity(power);
-        robot.flywheelTwo.setVelocity(power);
-        sleep(3000);
-        robot.feedServo.setPosition(0);//number 1
-        sleep(2000);
-        robot.feedServo.setPosition(1);
-        robot.flywheelOne.setPower(0);
-        robot.flywheelTwo.setPower(0);
-        sleep(1000);
-        robot.intake.setPower(0.5);
-        sleep(250);
-        robot.intake.setPower(-1);
-        sleep(500);
-        robot.intake.setPower(0);//end block
+        flywheelOne.setVelocity(0);
+        flywheelTwo.setVelocity(0);
 
-        robot.flywheelOne.setVelocity(power);
-        robot.flywheelTwo.setVelocity(power);
-        sleep(3000);
-        robot.feedServo.setPosition(0);//number 1
-        sleep(2000);
-        robot.feedServo.setPosition(1);
-        robot.flywheelOne.setPower(0);
-        robot.flywheelTwo.setPower(0);
-        sleep(1000);
-        robot.intake.setPower(0.5);
-        sleep(250);
-        robot.intake.setPower(-1);
-        sleep(500);
-        robot.intake.setPower(0);//end blo ck
+        // ================= DRIVE FORWARD =================
+        double distanceInches = 18; // 1.5 feet (change to 12 for 1 foot)
+        int moveTicks = (int) (distanceInches * TICKS_PER_INCH);
 
+        robot.frontLeftDrive.setTargetPosition(moveTicks);
+        robot.frontRightDrive.setTargetPosition(moveTicks);
+        robot.backLeftDrive.setTargetPosition(moveTicks);
+        robot.backRightDrive.setTargetPosition(moveTicks);
 
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        robot.frontLeftDrive.setPower(0.5);
+        robot.frontRightDrive.setPower(0.5);
+        robot.backLeftDrive.setPower(0.5);
+        robot.backRightDrive.setPower(0.5);
 
+        while (opModeIsActive() &&
+                robot.frontLeftDrive.isBusy() &&
+                robot.frontRightDrive.isBusy()) {
+            idle();
+        }
 
-
-//        forward(250,1);iyt
-//        sleep(500);
-//        forward(250,-1);
-//        robot.feedServo.setPosition(0);//number 2
+        robot.frontLeftDrive.setPower(0);
+        robot.frontRightDrive.setPower(0);
+        robot.backLeftDrive.setPower(0);
+        robot.backRightDrive.setPower(0);
+    }
+}
+//package org.firstinspires.ftc.teamcode.Autonomous;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
+//import org.firstinspires.ftc.teamcode.HWMap;
+//
+////name that appeafrss on the driver hub screen
+//@Autonomous(name = "shootinplace")
+//public class auton extends LinearOpMode {
+//    //making a robot from project file (hardware map)
+//    public HWMap robot = new HWMap();
+//    double power = 950;
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//        //initialize hardware map
+//        robot.init(hardwareMap);// start block
+//        waitForStart();
+//        robot.flywheelOne.setVelocity(power);
+//        robot.flywheelTwo.setVelocity(power);
 //        sleep(3000);
+//        robot.feedServo.setPosition(0);//number 1
+//        sleep(2000);
 //        robot.feedServo.setPosition(1);
+//        robot.flywheelOne.setPower(0);
+//        robot.flywheelTwo.setPower(0);
+//        sleep(1000);
+//        robot.intake.setPower(0.5);
+//        sleep(250);
+//        robot.intake.setPower(-0.5);
+//        sleep(500);
+//        robot.intake.setPower(0);//end block
+//
+//
+//        robot.flywheelOne.setVelocity(power);
+//        robot.flywheelTwo.setVelocity(power);
 //        sleep(3000);
+//        robot.feedServo.setPosition(0);//number 1
+//        sleep(2000);
+//        robot.feedServo.setPosition(1);
+//        robot.flywheelOne.setPower(0);
+//        robot.flywheelTwo.setPower(0);
+//        sleep(1000);
 //        robot.intake.setPower(0.5);
 //        sleep(250);
 //        robot.intake.setPower(-1);
-//        sleep(2000);
-//        forward(250,1); out
 //        sleep(500);
-//        forward(250,-1);
-//        robot.intake.setPower(-0);
-//        sleep(1000);
-//        robot.feedServo.setPosition(0);//number 3
+//        robot.intake.setPower(0);//end block
+//
+//        robot.flywheelOne.setVelocity(power);
+//        robot.flywheelTwo.setVelocity(power);
+//        sleep(3000);
+//        robot.feedServo.setPosition(0);//number 1
 //        sleep(2000);
+//        robot.feedServo.setPosition(1);
 //        robot.flywheelOne.setPower(0);
 //        robot.flywheelTwo.setPower(0);
-//        robot.feedServo.setPosition(0);
-        robot.backRightDrive.setPower(-0.7);
-        robot.backLeftDrive.setPower(-0.7);
-        robot.frontRightDrive.setPower(-0.7);
-        robot.frontLeftDrive.setPower(-0.7);
-        sleep(4000);
-        stopall();
-    }
-
-    public void forward(int time, double speed) { //forward
-        robot.backRightDrive.setPower(speed);
-        robot.backLeftDrive.setPower(speed);
-        robot.frontRightDrive.setPower(speed);
-        robot.frontLeftDrive.setPower(speed);
-        sleep(time);
-        stopall();
-    }
-    public void stopall() { //forward
-        robot.backRightDrive.setPower(0);
-        robot.backLeftDrive.setPower(0);
-        robot.frontRightDrive.setPower(0);
-        robot.frontLeftDrive.setPower(0);
-    }
-
-}
+//        sleep(1000);
+//        robot.intake.setPower(0.5);
+//        sleep(250);
+//        robot.intake.setPower(-1);
+//        sleep(500);
+//        robot.intake.setPower(0);//end blo ck
+//
+//
+//
+//
+//
+//
+////        forward(250,1);iyt
+////        sleep(500);
+////        forward(250,-1);
+////        robot.feedServo.setPosition(0);//number 2
+////        sleep(3000);
+////        robot.feedServo.setPosition(1);
+////        sleep(3000);
+////        robot.intake.setPower(0.5);
+////        sleep(250);
+////        robot.intake.setPower(-1);
+////        sleep(2000);
+////        forward(250,1); out
+////        sleep(500);
+////        forward(250,-1);
+////        robot.intake.setPower(-0);
+////        sleep(1000);
+////        robot.feedServo.setPosition(0);//number 3
+////        sleep(2000);
+////        robot.flywheelOne.setPower(0);
+////        robot.flywheelTwo.setPower(0);
+////        robot.feedServo.setPosition(0);
+//        robot.backRightDrive.setPower(-0.7);
+//        robot.backLeftDrive.setPower(-0.7);
+//        robot.frontRightDrive.setPower(-0.7);
+//        robot.frontLeftDrive.setPower(-0.7);
+//        sleep(4000);
+//        stopall();
+//    }
+//
+//    public void forward(int time, double speed) { //forward
+//        robot.backRightDrive.setPower(speed);
+//        robot.backLeftDrive.setPower(speed);
+//        robot.frontRightDrive.setPower(speed);
+//        robot.frontLeftDrive.setPower(speed);
+//        sleep(time);
+//        stopall();
+//    }
+//    public void stopall() { //forward
+//        robot.backRightDrive.setPower(0);
+//        robot.backLeftDrive.setPower(0);
+//        robot.frontRightDrive.setPower(0);
+//        robot.frontLeftDrive.setPower(0);
+//    }
+//
+//}
